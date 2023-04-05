d52a13e35404c1834e5a3307da1313c2211a3a48
Roope Vuorenlehto <rvuorenl@home.fi>
2023-04-05 19:34:56 +0300

    add week1, credit

---
+++ week1/credit.c
@@ -0,0 +1,210 @@
+#include "../includes/libft.h"
+
+int		all_digits(char *str);
+int		num_length(long num);
+void	print_invalid(void);
+int		checksum(long num, int length);
+void	check_visa(long num, int length);
+void	check_amex(long num, int length);
+void	check_master_or_visa(long num, int length);
+long	ft_atol(const char *str);
+
+int	main(void)
+{
+	char	*input;
+	long	num;
+	int		length;
+
+	while (1)
+	{
+		ft_printf("Number: ");
+		get_next_line(0, &input);
+
+		if (all_digits(input))
+		{
+			num = ft_atol(input);
+			ft_strdel(&input);
+			break ;
+		}
+		ft_strdel(&input);
+	}
+
+	length = num_length(num);
+
+	if (length == 15)
+		check_amex(num, length);
+	else if (length == 13)
+		check_visa(num, length);
+	else if (length == 16)
+		check_master_or_visa(num, length);
+	else
+		print_invalid();
+	return (0);
+}
+
+void	check_master_or_visa(long num, int length)
+{
+	int		pos;
+	long	copy;
+	int		second;
+
+	pos = 0;
+	copy = num;
+	while (pos < 14)
+	{
+		copy /= 10;
+		pos++;
+	}
+	if (copy / 10 == 4)
+	{
+		if (checksum(num, length))
+			ft_printf("VISA\n");
+	}
+	else if (copy / 10 == 5)
+	{
+		second = copy % 10;
+		if (second >= 1 && second <= 5)
+		{
+			if (checksum(num, length))
+				ft_printf("MASTERCARD\n");
+		}
+		else
+			print_invalid();
+	}
+	else
+		print_invalid();
+}
+
+int	sum_digits(long num)
+{
+	int result;
+	int	total;
+
+	result = (num % 10) * 2;
+	if (result < 10)
+		return (result);
+	else
+	{
+		total = result % 10;
+		result /= 10;
+		total += result;
+		return (total);
+	}
+}
+
+int	checksum(long num, int length)
+{
+	long 	num_copy;
+	int		total;
+	int		len_copy;
+	int		modulo;
+
+	total = 0;
+	num_copy = num;
+
+	len_copy = length - 1;
+	while (len_copy > 0)
+	{
+		num_copy /= 10;
+		total += sum_digits(num_copy);
+		num_copy /= 10;
+		len_copy -= 2;
+	}
+
+	len_copy = length;
+	num_copy = num;
+	while (len_copy > 0)
+	{
+		total += num_copy % 10;
+		num_copy /= 10;
+		num_copy /= 10;
+		len_copy -= 2;
+	}
+
+	if (total % 10 != 0)
+	{
+		print_invalid();
+		return (0);
+	}
+	return (1);
+}
+
+void	check_visa(long num, int length)
+{
+	int		pos;
+	long	copy;
+
+	pos = 0;
+	copy = num;
+	while (pos < 12)
+	{
+		copy /= 10;
+		pos++;
+	}
+	if (copy == 4)
+	{
+		if (checksum(num, length))
+			ft_printf("VISA\n");
+	}
+	else
+		print_invalid();
+}
+
+void	check_amex(long num, int length)
+{
+	int		pos;
+	long	copy;
+
+	copy = num;
+	pos = 0;
+	while (pos < 13)
+	{
+		copy /= 10;
+		pos++;
+	}
+	if (copy % 10 == 7 || copy % 10 == 4)
+	{
+		copy /= 10;
+		if (copy % 10 == 3)
+		{
+			if (checksum(num, length))
+				ft_printf("AMEX\n");
+		}
+		else
+			print_invalid();
+	}
+	else
+		print_invalid();
+}
+
+void	print_invalid(void)
+{
+	ft_printf("INVALID\n");
+}
+
+int	num_length(long num)
+{
+	int	result;
+
+	result = 0;
+	while (num != 0)
+	{
+		num /= 10;
+		result++;
+	}
+	return (result);
+}
+
+int	all_digits(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return (0);
+		i++;
+	}
+	return (1);
+}
---
+++ week1/ft_atol.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skattankakku <skattankakku@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 20:18:18 by rvuorenl          #+#    #+#             */
+/*   Updated: 2023/04/05 19:29:06 by skattankakk      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+
+static void	set_negative(int *negative, char c, int *i)
+{
+	if (c == '-')
+		*negative = -1;
+	(*i)++;
+}
+
+long	ft_atol(const char *str)
+{
+	unsigned long	num;
+	int				i;
+	int				negative;
+
+	if (!str)
+		ft_exit_error("ft_atol.c", 299);
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\f' || str[i] == '\v' || str[i] == '\r')
+		i++;
+	num = 0;
+	negative = 1;
+	if (str[i] == '-' || str[i] == '+')
+		set_negative(&negative, str[i], &i);
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		num = num * 10 + (str[i++] - '0');
+		if (negative == 1 && num > 9223372036854775807)
+			return (-1);
+		if (negative == -1 && num >= 9223372036854775808u)
+			return (0);
+	}
+	return (num * (negative));
+}
