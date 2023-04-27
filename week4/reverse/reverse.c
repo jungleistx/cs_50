0f38e2c17f15349c736e3ca0b6cc323e1201c32a
Roope Vuorenlehto <rvuorenl@home.fi>
2023-04-28 01:31:13 +0300

    mod reverse, add todo6

--- week4/reverse/reverse.c
+++ week4/reverse/reverse.c
@@ -54,6 +54,7 @@ int main(int argc, char *argv[])
 
     // Write header to file
     // TODO #6
+    write(output_fd, &header, sizeof(WAVHEADER));
 
     // Use get_block_size to calculate size of block
     // TODO #7
