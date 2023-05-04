c93f76de67ea501b7ce76c038012a718671de017
Roope Vuorenlehto <rvuorenl@home.fi>
2023-05-04 21:31:52 +0300

    mod cash, remove testing forever-while loop

--- week6/sentimental-cash/cash.py
+++ week6/sentimental-cash/cash.py
@@ -7,19 +7,18 @@ def get_float(str):
 	except ValueError:
 		return 0.0
 
-while True:
-	num = 0.0
-	while num <= 0.0:			# get posivive input
-		str = input("Change owed: ")
-		num = get_float(str)
+num = 0.0
+while num <= 0.0:			# get posivive input
+	str = input("Change owed: ")
+	num = get_float(str)
 
-	num *= 100			# avoid the floating-point rounding errors
-	coin_amount = 0
-	coins = [25, 10, 5, 1]
+num *= 100			# avoid the floating-point rounding errors
+coin_amount = 0
+coins = [25, 10, 5, 1]
 
-	for i in range(len(coins)):
-		while num >= coins[i]:
-			num -= coins[i]
-			coin_amount += 1
+for i in range(len(coins)):
+	while num >= coins[i]:
+		num -= coins[i]
+		coin_amount += 1
 
-	print(coin_amount)
+print(coin_amount)
