27a24acb73a1935870459a3053a891c691adddcd
Roope Vuorenlehto <rvuorenl@home.fi>
2023-05-04 21:31:08 +0300

    mod cash, more compact version using loop and pre-defined array of coins

--- week6/sentimental-cash/cash.py
+++ week6/sentimental-cash/cash.py
@@ -7,28 +7,19 @@ def get_float(str):
 	except ValueError:
 		return 0.0
 
-num = 0.0
-while num <= 0.0:			# get posivive input
-	str = input("Change owed: ")
-	num = get_float(str)
+while True:
+	num = 0.0
+	while num <= 0.0:			# get posivive input
+		str = input("Change owed: ")
+		num = get_float(str)
 
-num *= 100			# avoid the floating-point rounding errors
-coins = 0
+	num *= 100			# avoid the floating-point rounding errors
+	coin_amount = 0
+	coins = [25, 10, 5, 1]
 
-while num >= 25:
-	num -= 25
-	coins += 1
+	for i in range(len(coins)):
+		while num >= coins[i]:
+			num -= coins[i]
+			coin_amount += 1
 
-while num >= 10:
-	num -= 10
-	coins += 1
-
-while num >= 5:
-	num -= 5
-	coins += 1
-
-while num >= 1:
-	num -= 1
-	coins += 1
-
-print(coins)
+	print(coin_amount)
