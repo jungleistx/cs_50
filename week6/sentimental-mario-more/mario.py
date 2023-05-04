c224306df6d67cbdde125d52bbecec8cb172b566
Roope Vuorenlehto <rvuorenl@home.fi>
2023-05-04 18:32:13 +0300

    mod mario, final program

--- week6/sentimental-mario-more/mario.py
+++ week6/sentimental-mario-more/mario.py
@@ -7,4 +7,14 @@ def get_int(str):
 	except ValueError:
 		return 0
 
+height = 0
+while height <= 0 or height > 8:		# get proper height (1-8)
+	height = get_int(input("Height: "))
 
+i = 1
+while i <= height:
+	print(" " * (height - i), end="")	# left padding
+	print("#" * i, end="")				# left side
+	print("  ", end="")					# middle gap
+	print("#" * i)						# right side
+	i += 1
